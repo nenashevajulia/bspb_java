8e3585cfc806ddadbeddca64f74d043964902fc4
nenashevajulia <44815321+nenashevajulia@users.noreply.github.com>
2021-09-21 14:11:35 +0300

    homework_lesson_2

---
+++ src/AnagramsCheck.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class AnagramsCheck {
+    public static void main(String[] args) {
+        String firstStr = "AnaGRa m-s";
+        String secondStr = "Ma-G NAs ra";
+
+        String firstVar = firstStr.toLowerCase().replace(" ", "");
+        String secondVar = secondStr.toLowerCase().replace(" ", "");
+
+        if (firstVar.length() != secondVar.length()) {
+            System.out.println("Слова " + firstStr + " и " + secondStr + " - не анаграмма");
+        } else {
+            char[] firstArray = firstVar.toCharArray();
+            char[] secondArray = secondVar.toCharArray();
+            Arrays.sort(firstArray);
+            Arrays.sort(secondArray);
+            if (Arrays.equals(firstArray, secondArray)) {
+                System.out.println("Слова " + firstStr + " и " + secondStr + " - анаграмма");
+            } else {
+                System.out.println("Слова " + firstStr + " и " + secondStr + " - не анаграмма");
+            }
+
+
+        }
+
+
+    }
+}
