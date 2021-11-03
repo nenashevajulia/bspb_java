d426ec204e456d62d73177fa75e5a452f86a5c4a
nenashevajulia <44815321+nenashevajulia@users.noreply.github.com>
2021-11-03 13:59:39 +0300

    homework_lesson_8

--- src/clientsFlUlSort/ClientsFlUlSort.java
+++ src/clientsFlUlSort/ClientsFlUlSort.java
@@ -1,5 +1,6 @@
 package clientsFlUlSort;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -7,11 +8,11 @@ public class ClientsFlUlSort {
     public static void main(String[] args) {
 
         DataClient dataClient = new DataClient();
-        Client[] clients = dataClient.createClients(10);
+        ArrayList<Client> clients = dataClient.createClients(100);
 
         int resultLength = 0;
-        for (int i = 0; i < clients.length; i++) {
-            var client = clients[i];
+        for (int i = 0; i < clients.size(); i++) {
+            var client = clients.get(i);
             if (client instanceof Individual) {
                 if (((Individual) client).getGender().equals("male")) {
                     resultLength++;
@@ -21,8 +22,8 @@ public class ClientsFlUlSort {
 
         Individual[] individualClients = new Individual[resultLength];
         int j = 0;
-        for (int i = 0; i < clients.length; i++) {
-            var client = clients[i];
+        for (int i = 0; i < clients.size(); i++) {
+            var client = clients.get(i);
             if (client instanceof Individual) {
                 if (((Individual) client).getGender().equals("male")) {
                     individualClients[j] = (Individual) client;
@@ -42,3 +43,37 @@ public class ClientsFlUlSort {
     }
 
 }
+
+//        Client[] clients = dataClient.createClients(10);
+//
+//        int resultLength = 0;
+//        for (int i = 0; i < clients.length; i++) {
+//            var client = clients[i];
+//            if (client instanceof Individual) {
+//                if (((Individual) client).getGender().equals("male")) {
+//                    resultLength++;
+//                }
+//            }
+//        }
+//
+//        Individual[] individualClients = new Individual[resultLength];
+//        int j = 0;
+//        for (int i = 0; i < clients.length; i++) {
+//            var client = clients[i];
+//            if (client instanceof Individual) {
+//                if (((Individual) client).getGender().equals("male")) {
+//                    individualClients[j] = (Individual) client;
+//                    j++;
+//                }
+//            }
+//        }
+//
+//        Arrays.sort(individualClients, new Comparator<Individual>() {
+//            @Override
+//            public int compare(Individual o1, Individual o2) {
+//                return o1.getYear() - o2.getYear();
+//            }
+//        });
+//
+//        System.out.println(Arrays.deepToString(individualClients));
+//    }
--- src/clientsFlUlSort/DataClient.java
+++ src/clientsFlUlSort/DataClient.java
@@ -1,21 +1,28 @@
 package clientsFlUlSort;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class DataClient {
 
-    public Client[] createClients(int countClients) {
+    public ArrayList<Client> createClients(int size) {
 
         ClientGenerator individualGenerator = new IndividualGenerator();
         ClientGenerator organizationGenerator = new OrganizationGenerator();
 
-        Client[] clients = new Client[countClients];
-        Random random = new Random();
-        for (int i = 0; i < countClients; i++) {
-            clients[i] = random.nextBoolean() ? individualGenerator.generateClient() : organizationGenerator.generateClient();
 
+        ArrayList<Client> list = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            list.add(i, random.nextBoolean() ? individualGenerator.generateClient() : organizationGenerator.generateClient());
         }
-        return clients;
+
+//        Client[] clients = new Client[countClients];
+//        Random random = new Random();
+//        for (int i = 0; i < countClients; i++) {
+//            clients[i] = random.nextBoolean() ? individualGenerator.generateClient() : organizationGenerator.generateClient();
+//        }
+        return list;
 
     }
 }
